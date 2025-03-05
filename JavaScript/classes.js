8238672c1d69aeb50a9ae665124a5b60659be7e5
Seminarfach <seminarfach2026@gmail.com>
2025-03-05 20:37:31 +0100

    Update classes.js

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -139,7 +139,7 @@ class ZoneSchulhof {
         const drawY = canvasCenterY - backgroundAnchor.y + this.position.y;
 
         // Zeichne die Zone
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillStyle = 'rgba(255, 0, 0, 0)';
         ctx.fillRect(drawX, drawY, this.width, this.height);
     }
 }
@@ -172,7 +172,7 @@ class ZoneFlur {
         const drawY = canvasCenterY - backgroundAnchor.y + this.position.y;
 
         // Zeichne die Zone
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillStyle = 'rgba(255, 0, 0, 0)';
         ctx.fillRect(drawX, drawY, this.width, this.height);
     }
 }
@@ -205,7 +205,7 @@ class ZoneRaum {
         const drawY = canvasCenterY - backgroundAnchor.y + this.position.y;
 
         // Zeichne die Zone
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillStyle = 'rgba(255, 0, 0, 0)';
         ctx.fillRect(drawX, drawY, this.width, this.height);
     }
 }
